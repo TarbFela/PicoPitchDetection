c400999d3500814ae74d974c18953cfae71fa8a3
Maxim LaPlante <maxim.laplante@maine.edu>
2025-05-02 09:21:10 -0400

    Cleanup

--- usb_debug.c
+++ usb_debug.c
@@ -24,21 +24,13 @@ int reset_pin_check() {
     return ((sio_hw->gpio_in & (1<<RESET_PIN)) != 0);
 }
 
-
-
-#define YIN_WINDOW_WIDTH_MS 20
-#define ADC_SAMPLE_RATE_HZ 15000
-
+// 20ms lets us see (theoretically) 50Hz
+// 15kHz gives us enough granularity to differentiate higher pitches
+#define YIN_WINDOW_WIDTH_MS 20 // how many ms of audio do we want to analyze? See Nyquist freq sampling.
+#define ADC_SAMPLE_RATE_HZ 15000 // what's our sample rate? See Nyquist time sampling
 #define AUDIO_BUFFER_SIZE          (YIN_WINDOW_WIDTH_MS * ADC_SAMPLE_RATE_HZ / 1000)
 
-
-
-// —————————————————————————————————————————————————————————————————————————————————————————
-//                                         MULTICORE CODE
-// —————————————————————————————————————————————————————————————————————————————————————————
-
 #define MULTICORE_GOOD_FLAG 0xBEEF
-
 void core1_entry() {
 
     // initialize...
@@ -81,13 +73,11 @@ void core1_entry() {
 
 }
 
-
-uint dma_chan; // globally accessible! May pose an issue for syntax...
 int main() {
 
-    // —————————————————————————————————————————————————————————————————————————————————————————
-    //                                         INITS
-    // —————————————————————————————————————————————————————————————————————————————————————————
+    // —————————————————————————————————————————————————————————————————————————————————————————//
+    //                                         INITS                                            //
+    // —————————————————————————————————————————————————————————————————————————————————————————//
 
     stdio_init_all();
     // RESET PIN
@@ -99,10 +89,8 @@ int main() {
 //                                ||         ADC INIT       ||
 //                                +——————————————————————————+
 
-
     adc_stream_t *adc_stream = init_adc_stream(ADC_GPIO_PIN, ADC_SAMPLE_RATE_HZ, AUDIO_BUFFER_SIZE);
-
-
+    sleep_ms(1000);
 
 //                                +——————————————————————————+
 //                                ||     MULTICORE INIT     ||
@@ -118,29 +106,25 @@ int main() {
     FREQ_ANALYZER_T *fa_s;
     fa_s = init_freq_analyzer(adc_stream->buffer, adc_stream->buffer_size, adc_stream->adc_sample_rate, 50);
     multicore_fifo_push_blocking((uint32_t)fa_s);
-
     sleep_ms(1000);
 
-    // —————————————————————————————————————————————————————————————————————————————————————————
-    //                                      MAIN LOOP
-    // —————————————————————————————————————————————————————————————————————————————————————————
+    // —————————————————————————————————————————————————————————————————————————————————————————//
+    //                                      MAIN LOOP                                           //
+    // —————————————————————————————————————————————————————————————————————————————————————————//
 
     char ui[64];
     for(int i = 0; i<63; i++) ui[i] = '.';
     ui[63] = 0;
 
-
     while(1) {
         scanf("%s",&ui);
         if(ui[0] == 'q') break;
         audio_capture_no_blocking(adc_stream); //start capture
-        dma_channel_wait_for_finish_blocking(dma_chan); //wait for end of capture
+        dma_channel_wait_for_finish_blocking(adc_stream->dma_channel); //wait for end of capture
         multicore_fifo_push_blocking(0); //start printout
         multicore_fifo_pop_blocking(); //wait for end of printout
     }
 
     reset_usb_boot(0,0);
     return 0;
-
-
 }
